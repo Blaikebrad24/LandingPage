6328741779c29579b2070b1d0f1337ce77c94ebc
Blaike Bradford <bb494v@att.com>
2022-06-12 13:03:30 -0500

    First commit to Landing Page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React, {Fragment} from 'react'
 import Navbar from '../components/NavBar'
 
+//Entry point of the web application
 export default function Home() {
   return (
     <div id ='scroll'>
